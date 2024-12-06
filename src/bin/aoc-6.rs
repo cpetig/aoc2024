7da8cb7130f62f4b29c28f025f4fef8ec34672c6
Christof Petig <christof.petig@arcor.de>
2024-12-07 00:15:59 +0100

    2nd part

--- src/bin/aoc-6.rs
+++ src/bin/aoc-6.rs
@@ -17,6 +17,7 @@ fn main() -> io::Result<()> {
             board.push(Vec::from(line));
         }
     }
+    let initial_position = position;
     assert!(board[position.1][position.0] == b'^');
     board[position.1][position.0] = b'X';
     count += 1;
@@ -47,10 +48,63 @@ fn main() -> io::Result<()> {
             _ => unreachable!(),
         }
     }
-    for l in board.iter() {
-        println!("{}", std::str::from_utf8(l).unwrap());
-    }
+    // for l in board.iter() {
+    //     println!("{}", std::str::from_utf8(l).unwrap());
+    // }
     // dbg!(board);
     println!("{count}");
+    let mut new_obstacles = 0;
+    for y in 0..board.len() {
+        for x in 0..board[0].len() {
+            if board[y][x] == b'X' && (x, y) != initial_position {
+                // potential loop
+                let mut testboard = board.clone();
+                testboard[y][x] = b'#';
+                position = initial_position;
+                direction = (0, -1);
+                let is_loop = loop {
+                    let newpos = (
+                        position.0 as isize + direction.0,
+                        position.1 as isize + direction.1,
+                    );
+                    if newpos.0 < 0
+                        || newpos.0 as usize >= board[0].len()
+                        || newpos.1 < 0
+                        || newpos.1 as usize >= board.len()
+                    {
+                        // for l in testboard.iter() {
+                        //     println!("{}", std::str::from_utf8(l).unwrap());
+                        // }
+                        break false;
+                    }
+                    let newpos = (newpos.0 as usize, newpos.1 as usize);
+                    match testboard[newpos.1][newpos.0] {
+                        b'.' | b'X' | b'u' | b'd' | b'l' | b'r' => {
+                            const DIRECTION: &[u8] = b"ul*rd";
+                            let dir_code = DIRECTION[(direction.0 + direction.1 * 2 + 2) as usize];
+                            if testboard[newpos.1][newpos.0] == dir_code {
+                                // dbg!(dir_code);
+                                break true;
+                            }
+                            testboard[newpos.1][newpos.0] = dir_code;
+                            position = newpos;
+                        }
+                        b'#' => {
+                            // rotate right
+                            direction = (-direction.1, direction.0);
+                        }
+                        _ => unreachable!(),
+                    }
+                };
+                if is_loop {
+                    // for l in testboard.iter() {
+                    //     println!("{}", std::str::from_utf8(l).unwrap());
+                    // }
+                    new_obstacles += 1;
+                }
+            }
+        }
+    }
+    println!("{new_obstacles}");
     Ok(())
 }
