f5c44cde0a0988dbaa385f5d0b568ab103d10dea
Christof Petig <christof.petig@arcor.de>
2024-12-11 15:24:01 +0100

    this completes in proper time

--- src/bin/aoc-11.rs
+++ src/bin/aoc-11.rs
@@ -1,44 +1,68 @@
-use std::io::{self, stdin, BufRead, BufReader};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    io::{self, stdin, BufRead, BufReader},
+};
 
-fn mutate(pebbles: &mut Vec<usize>) {
-    let mut input: Vec<usize> = Vec::new();
-    std::mem::swap(pebbles, &mut input);
-    for i in input.drain(0..) {
-        if i == 0 {
-            pebbles.push(1);
-        } else {
-            let digits = i.ilog10() + 1;
-            if digits & 1 == 0 {
-                let break_at = usize::pow(10, digits / 2);
-                pebbles.push(i / break_at);
-                pebbles.push(i % break_at);
-            } else {
-                pebbles.push(i * 2024);
-            }
-        }
-    }
-}
+// fn mutate(pebbles: &mut Vec<usize>) {
+//     let mut input: Vec<usize> = Vec::new();
+//     std::mem::swap(pebbles, &mut input);
+//     for i in input.drain(0..) {
+//         if i == 0 {
+//             pebbles.push(1);
+//         } else {
+//             let digits = i.ilog10() + 1;
+//             if digits & 1 == 0 {
+//                 let break_at = usize::pow(10, digits / 2);
+//                 pebbles.push(i / break_at);
+//                 pebbles.push(i % break_at);
+//             } else {
+//                 pebbles.push(i * 2024);
+//             }
+//         }
+//     }
+// }
 
-fn pebbles_after(start: usize, count: usize) -> usize {
+// first is start value, then how many iterations left
+type Cache = HashMap<usize, Vec<usize>>;
+
+fn pebbles_after(start: usize, count: usize, cache: &mut Cache) -> usize {
     // dbg!((start,count));
     if count == 0 {
         1
     } else if start == 0 {
-        pebbles_after(1, count - 1)
+        pebbles_after(1, count - 1, cache)
     } else {
-        let digits = start.ilog10() + 1;
-        if digits & 1 == 0 {
-            let break_at = usize::pow(10, digits / 2);
-            pebbles_after(start / break_at, count - 1) + pebbles_after(start % break_at, count - 1)
-        } else {
-            pebbles_after(2024 * start, count - 1)
+        let entry = cache.entry(start);
+        if let Entry::Occupied(item) = &entry {
+            if let Some(value) = item.get().get(count - 1) {
+                if *value > 0 {
+                    return *value;
+                }
+            }
         }
+        entry.or_insert(Vec::from_iter(std::iter::repeat_n(0, count)));
+        let digits = start.ilog10() + 1;
+        let value = if digits & 1 == 0 {
+            let break_at = usize::pow(10, digits / 2);
+            pebbles_after(start / break_at, count - 1, cache)
+                + pebbles_after(start % break_at, count - 1, cache)
+        } else {
+            pebbles_after(2024 * start, count - 1, cache)
+        };
+        let entry = cache.entry(start);
+        entry.and_modify(|vec| {
+            if vec.len() < count {
+                vec.extend(std::iter::repeat_n(0, count - vec.len()));
+            };
+            vec[count - 1] = value
+        });
+        value
     }
 }
 
 fn main() -> io::Result<()> {
     let reader = BufReader::new(stdin());
-    let mut pebbles: Vec<usize> = reader
+    let pebbles: Vec<usize> = reader
         .lines()
         .next()
         .unwrap()
@@ -52,9 +76,10 @@ fn main() -> io::Result<()> {
     //     // dbg!(&pebbles);
     // }
     // println!("{}", pebbles.len());
-    let mut pebble_count=0;
+    let mut pebble_count = 0;
+    let mut cache = Cache::new();
     for i in pebbles.iter() {
-        pebble_count+=pebbles_after(*i, 75);
+        pebble_count += pebbles_after(*i, 75, &mut cache);
     }
     println!("{pebble_count}");
     Ok(())
